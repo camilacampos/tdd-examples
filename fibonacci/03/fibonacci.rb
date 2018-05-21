d69598bcbaae73a3e3acdb03b16195281faaa141
camilacampos <camilacamposramos.si@gmail.com>
2018-05-20 23:50:42 -0300

    third test and code for fibonacci
    Signed-off-by: camilacampos <camilacamposramos.si@gmail.com>

---
+++ fibonacci/03/fibonacci.rb
@@ -0,0 +1,7 @@
+class Fibonacci
+  def until(last_index)
+    return 1 if last_index < 3
+
+    2
+  end
+end
---
+++ fibonacci/03/fibonacci_spec.rb
@@ -0,0 +1,21 @@
+require_relative 'fibonacci'
+
+RSpec.describe Fibonacci do
+  it 'returns 1 as first number' do
+    result = described_class.new.until(1)
+
+    expect(result).to eq 1
+  end
+
+  it 'returns 1 as second number' do
+    result = described_class.new.until(2)
+
+    expect(result).to eq 1
+  end
+
+  it 'returns 2 as third number' do
+    result = described_class.new.until(3)
+
+    expect(result).to eq 2
+  end
+end
