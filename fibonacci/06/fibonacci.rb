04a67e5768702d8b54ebb351bdd1ae33bef65eb3
camilacampos <camilacamposramos.si@gmail.com>
2018-05-21 23:02:06 -0300

    refactor fibonnaci code
    Signed-off-by: camilacampos <camilacamposramos.si@gmail.com>

--- fibonacci/06/fibonacci.rb
+++ fibonacci/06/fibonacci.rb
@@ -1,14 +1,12 @@
 class Fibonacci
   def until(last_index)
-    return 1 if last_index < 3
+    current_number = 0
+    last_number = 1
 
-    last_number = 0
-    current_number = 1
-
-    (last_index - 1).times do
-      tmp = last_number
-      last_number = current_number
-      current_number = tmp + last_number
+    last_index.times do
+      tmp = current_number
+      current_number = last_number
+      last_number = tmp + current_number
     end
 
     current_number
