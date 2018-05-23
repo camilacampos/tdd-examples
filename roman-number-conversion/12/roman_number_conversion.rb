0d68b056a387d346e2b36453a02704fd958274d3
camilacampos <camilacamposramos.si@gmail.com>
2018-05-23 19:56:09 -0300

    [roman numbers] test for XXX to XXXIX
    Signed-off-by: camilacampos <camilacamposramos.si@gmail.com>

---
+++ roman-number-conversion/12/roman_number_conversion.rb
@@ -0,0 +1,17 @@
+class RomanNumberConversion
+  def from_int(integer)
+    return 'XXX' + convert(integer-30) if integer > 30
+    return 'XX' + convert(integer-20) if integer > 20
+    return 'X' + convert(integer-10) if integer > 10
+
+    convert(integer)
+  end
+
+  def convert(integer)
+    return 'I' * (10 - integer) + 'X' if integer > 8
+    return 'V' + ('I' * (integer - 5)) if integer > 5
+    return 'I' * (5 - integer) + 'V' if integer > 3
+
+    'I' * integer
+  end
+end
