b4f74a3042bc755fc9aab94d784d1ddae2554886
camilacampos <camilacamposramos.si@gmail.com>
2018-05-23 19:11:46 -0300

    [roman numbers] test for XIV and XV
    Signed-off-by: camilacampos <camilacamposramos.si@gmail.com>

---
+++ roman-number-conversion/08/roman_number_conversion.rb
@@ -0,0 +1,11 @@
+class RomanNumberConversion
+  def from_int(integer)
+    return 'X' + ('I' * (5 - (integer - 10))) + 'V' if integer > 13
+    return 'X' + ('I' * (integer - 10)) if integer > 10
+    return 'I' * (10 - integer) + 'X' if integer > 8
+    return 'V' + ('I' * (integer - 5)) if integer > 5
+    return 'I' * (5 - integer) + 'V' if integer > 3
+
+    'I' * integer
+  end
+end
