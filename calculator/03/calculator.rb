3cf625535414636877ab2701cdab74037e5b7f0c
juliabirkett <juliambirkett@gmail.com>
2018-05-23 21:56:46 -0300

    Calculator 03

---
+++ calculator/03/calculator.rb
@@ -0,0 +1,11 @@
+class Calculator
+  def calculate(x, y, operation:)
+    if operation.eql?('+')
+      return x + y
+    elsif operation.eql?('-')
+      return x - y
+    elsif operation.eql?('*')
+      return x * y
+    end
+  end
+end
